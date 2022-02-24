35895f38a51c6923b0ffccd7042c85148a4fdfa5
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-02-24 13:31:03 +0100

    Reaction buttons added

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -6,13 +6,15 @@ const initialState = [
     id: '1', 
     title: 'First Post!', 
     content: 'Hello!',
-    date: sub(new Date(), { minutes: 10 }).toISOString() 
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
   },
   { 
     id: '2', 
     title: 'Second Post', 
     content: 'More text',
-    date: sub(new Date(), { minutes: 5 }).toISOString() 
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
   }
 ]
 
@@ -31,7 +33,8 @@ const postsSlice = createSlice({
             date: new Date().toISOString(),
             title,
             content,
-            user: userId
+            user: userId,
+            reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
           }
         }
       }
@@ -43,10 +46,17 @@ const postsSlice = createSlice({
         exisitingPost.title = title;
         exisitingPost.content = content;
       }
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const exisitingPost = state.find(post => post.id === postId);
+      if (exisitingPost) {
+        exisitingPost.reactions[reaction]++
+      }
     }
   }
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions;
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
