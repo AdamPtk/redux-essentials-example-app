b9f0e09de04136c9eb1bf339b4dc1f31d19585bb
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-02-24 16:43:09 +0100

    Post adding

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -12,27 +12,33 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   return response.data
 })
 
+export const addNewPost = createAsyncThunk('posts/addNewPost', async initialPost => {
+  const response = await client.post('fakeApi/posts', initialPost)
+  return response.data
+})
+
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded: {
-      reducer(state, action) {
-        state.posts.push(action.payload)
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            date: new Date().toISOString(),
-            title,
-            content,
-            user: userId,
-            reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-          }
-        }
-      }
-    },
+    // EXTRA REDUCERS ADDED - addNewPost
+    // postAdded: {
+    //   reducer(state, action) {
+    //     state.posts.push(action.payload)
+    //   },
+    //   prepare(title, content, userId) {
+    //     return {
+    //       payload: {
+    //         id: nanoid(),
+    //         date: new Date().toISOString(),
+    //         title,
+    //         content,
+    //         user: userId,
+    //         reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+    //       }
+    //     }
+    //   }
+    // },
     postUpdated(state, action) {
       const { id, title, content } = action.payload;
       const exisitingPost = state.posts.find(post => post.id === id);
@@ -61,6 +67,10 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message
+      })
+    builder
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        state.posts.push(action.payload)
       })
   }
 })
